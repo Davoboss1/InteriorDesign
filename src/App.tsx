f190e3f4541c3f074291be2678e9d965bfc83eba
Davo <davidakinfenwa2@gmail.com>
2023-01-13 13:15:43 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,187 @@
+//React imports
 import { useState } from 'react';
+
+// Material ui imports
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+
+//Css imports
+import "./css/bootstrap-utilities.min.css"
+import "./css/App.css"
+
+//Images import
+import logo from "./assets/Logo.svg";
+
+const NavBar = () => {
+
+  const pages = ['Home', 'Pages', 'Services', 'Project', 'Blog', 'Contact'];
+
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+
+  return (
+    <AppBar position="static" className={"main-navbar"} >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters sx={{ ml: "15%", mr: "15%" }}>
+          <img src={logo} alt={"Inferno Logo"} height="24" width="24" />
+          <Typography
+            variant="h6"
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              ml: 2,
+              textDecoration: 'none',
+              fontFamily: "'DM Serif Display', serif"
+            }}
+          >
+            Interno
+          </Typography>
+
+          <Box className='d-none ms-auto d-md-flex'>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          {/* Small screen display */}
+          <Box className={"d-flex justify-content-end d-md-none w-100"}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center" >{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className='top-view'>
+        <div className='my-auto'>
+          <h1>
+            Let Your Home Be Unique
+          </h1>
+          <p>
+            Duis eiusmod officia eu officia minim in aliquip ea deserunt non velit veniam ipsum mollit.
+          </p>
+
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const Footer = () => {
+
+  return (
+    <footer>
+      <Grid container p={"5% 10%"} spacing={3}>
+        <Grid item xs={5}>
+          <div className='d-flex align-items-center'>
+            <img src={logo} alt={"Inferno Logo"} height="24" width="24" />
+          <Typography
+            variant="h6"
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              ml: 2,
+              textDecoration: 'none',
+              fontFamily: "'DM Serif Display', serif"
+            }}
+          >
+            Interno
+          </Typography>
+          </div>
+          <p>Non velit cillum ut duis magna reprehenderit amet reprehenderit magna sunt duis cupidatat eu et.</p>
+
+        
+        </Grid>
+
+        <Grid item xs>
+
+        </Grid>
+
+        <Grid item xs>
+
+        </Grid>
+
+        <Grid item xs={3}>
+
+        </Grid>
+
+      </Grid>
+    </footer>
+  );
+}
 
 function App() {
 
   return (
-     <h1>Hello world</h1>
+    <>
+      <NavBar />
+      <HomePage />
+      <Footer />
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { StyledEngineProvider } from '@mui/material/styles';
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      {/* So i can override MUI's styles. */}
+      <App />
+    </StyledEngineProvider>
   </React.StrictMode>,
 )
